e782883adc632d30bce1050bd347e2eab3d6660f
Meo-ICAR <piergiuseppe.meo@icar.cnr.it>
2026-04-13 16:27:40 +0200

    ok path

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,8 @@
 <?php
 
-use App\Livewire\PublicCompanyWelcome;
-use App\Livewire\PublicReportForm;  // Il componente Livewire creato in precedenza
-use App\Livewire\ReportTracker;
+use App\Livewire\PublicReportForm;
+use App\Livewire\PublicReportTracker;
 use Illuminate\Support\Facades\Route;
-
-// 1. Pagina di Benvenuto (Landing)
-Route::get('/segnalazioni/{company:slug}', PublicCompanyWelcome::class)
-    ->name('report.welcome');
-
-// 2. Form di inserimento
-Route::get('/segnalazioni/{company:slug}/nuova', PublicReportForm::class)
-    ->name('report.form');
-
-// 3. Verifica stato (Tracker)
-Route::get('/segnalazioni/{company:slug}/traccia', ReportTracker::class)
-    ->name('report.tracker');
 
 Route::redirect('/', '/admin');
 
@@ -25,4 +12,4 @@ Route::get('/home', function () {
 
 Route::get('/segnala/{company:slug}', PublicReportForm::class)
     ->name('report.form');
-Route::get('/tracker', \App\Livewire\PublicReportTracker::class)->name('report.tracker');
+Route::get('/tracker', PublicReportTracker::class)->name('report.tracker');
